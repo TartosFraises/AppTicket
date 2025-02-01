c015f24150f7df51c074968e6e1cbd7c76c79a30
Yann <leguennec@gmail.com>
2025-02-01 11:38:57 +0100

    Add dynamic sidebar, routes and empty template

--- app/routes/base.py
+++ app/routes/base.py
@@ -19,13 +19,21 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password_hash']
-        
-        print(f"{username} {password}")
-        if username == "toto" and password== "MfemXjFVhqwZi9eYtmKc5JA9CJlHbVdBqfMuLlIbamY=": # toto/toto
+        if username == "admin" and password== "MfemXjFVhqwZi9eYtmKc5JA9CJlHbVdBqfMuLlIbamY=": # admin/toto
             session['username'] = username
-            #session['userrole'] = 'user' 
+            session['userrole'] = 'admin'
+            user = User(username)
+            login_user(user)
+            return redirect(url_for('base_route.home'))
+        elif username == "user" and password== "MfemXjFVhqwZi9eYtmKc5JA9CJlHbVdBqfMuLlIbamY=": # user/toto
+            session['username'] = username
+            session['userrole'] = 'user' 
+            user = User(username)
+            login_user(user)
+            return redirect(url_for('base_route.home'))
+        elif username == "tech" and password== "MfemXjFVhqwZi9eYtmKc5JA9CJlHbVdBqfMuLlIbamY=": # tech/toto
+            session['username'] = username
             session['userrole'] = 'technician'
-            #session['userrole'] = 'admin'
             user = User(username)
             login_user(user)
             return redirect(url_for('base_route.home'))
@@ -40,3 +48,63 @@ def logout():
     session.pop('username', None)
     logout_user()
     return redirect(url_for('base_route.login'))
+    
+@base_route.route('/about')
+@login_required
+def about():
+    if 'username' in session:
+        return render_template('about.html', username=session['username'], userrole = session['userrole'])
+    else:
+        return redirect(url_for('login'))
+
+@base_route.route('/tickets')
+@login_required
+def tickets():
+    if 'username' in session:
+        return render_template('tickets.html', username=session['username'], userrole = session['userrole'])
+    else:
+        return redirect(url_for('login'))
+        
+@base_route.route('/update_user')
+@login_required
+def update_user():
+    if 'username' in session:
+        return render_template('update_user.html', username=session['username'], userrole = session['userrole'])
+    else:
+        return redirect(url_for('login'))
+        
+        
+@base_route.route('/all_tickets')
+@login_required
+def all_tickets():
+    if 'username' in session:
+        return render_template('all_tickets.html', username=session['username'], userrole = session['userrole'])
+    else:
+        return redirect(url_for('login'))
+        
+@base_route.route('/config_users')
+@login_required
+def config_users():
+    if 'username' in session:
+        return render_template('config_users.html', username=session['username'], userrole = session['userrole'])
+    else:
+        return redirect(url_for('login'))
+
+
+
+@base_route.route('/assigned_tickets')
+@login_required
+def assigned_tickets():
+    if 'username' in session:
+        return render_template('assigned_tickets.html', username=session['username'], userrole = session['userrole'])
+    else:
+        return redirect(url_for('login'))
+
+
+@base_route.route('/statistics')
+@login_required
+def statistics():
+    if 'username' in session:
+        return render_template('statistics.html', username=session['username'], userrole = session['userrole'])
+    else:
+        return redirect(url_for('login'))

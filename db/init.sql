a87b9d711cd59754918de181a529007ba4653df3
ShuOnSSH <shu@MacBook-Air-de-ShuOnSSH.local>
2025-02-04 13:48:55 +0100

    Mise à jour de init.sql, requirements.txt et run.py

--- db/init.sql
+++ db/init.sql
@@ -16,48 +16,14 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `Users`
---
-
-DROP TABLE IF EXISTS `Users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Users` (
-  `user_id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `hashpassword` varchar(255) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `role` enum('user','technician','admin') NOT NULL,
-  `phone_number` varchar(15) NOT NULL,
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Dumping data for table `Users`
 --
 
 LOCK TABLES `Users` WRITE;
 /*!40000 ALTER TABLE `Users` DISABLE KEYS */;
-INSERT INTO `Users` VALUES (1,'titi','29352a41244860cacb6e5e9a258424537fb54fe5c29f2046633e8d53e12f4f1a','titi.lulu1@gmail.com','user','1234567890'),(2,'tata','6854723dee01a5d11f1a844a75c104f1f42fbe962bb44c555251dbf67e38f5df','tata.Celine2@gmail.com','technician','0987654321'),(3,'toto','045c32e82a1392ac2d141e417ed7cdb95588d1c63f398908a014c1d6ae582610','toto.Roger3@gmail.com','admin','1122334455');
+INSERT INTO `Users` VALUES (4,'admin','e10adc3949ba59abbe56e057f20f883e','admin_3241@example.com','admin','7392518476');
 /*!40000 ALTER TABLE `Users` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `categories`
---
-
-DROP TABLE IF EXISTS `categories`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `categories` (
-  `category_id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `description` text,
-  PRIMARY KEY (`category_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `categories`
@@ -70,32 +36,40 @@ INSERT INTO `categories` VALUES (1,'Rseau','Problmes lis aux connexions rseau'),
 UNLOCK TABLES;
 
 --
--- Table structure for table `tickets`
+-- Dumping data for table `knowledge_base`
 --
 
-DROP TABLE IF EXISTS `tickets`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tickets` (
-  `ticket_id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(100) NOT NULL,
-  `description` text,
-  `status` enum('open','in_progress','closed','waiting','bloque') DEFAULT 'open',
-  `priority` enum('low','medium','high','critical') DEFAULT 'low',
-  `created_by` int NOT NULL,
-  `assigned_to` int DEFAULT NULL,
-  `category_id` int NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`ticket_id`),
-  KEY `created_by` (`created_by`),
-  KEY `assigned_to` (`assigned_to`),
-  KEY `category_id` (`category_id`),
-  CONSTRAINT `tickets_ibfk_1` FOREIGN KEY (`created_by`) REFERENCES `Users` (`user_id`),
-  CONSTRAINT `tickets_ibfk_2` FOREIGN KEY (`assigned_to`) REFERENCES `Users` (`user_id`),
-  CONSTRAINT `tickets_ibfk_3` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `knowledge_base` WRITE;
+/*!40000 ALTER TABLE `knowledge_base` DISABLE KEYS */;
+/*!40000 ALTER TABLE `knowledge_base` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `notifications`
+--
+
+LOCK TABLES `notifications` WRITE;
+/*!40000 ALTER TABLE `notifications` DISABLE KEYS */;
+/*!40000 ALTER TABLE `notifications` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `ticket_comments`
+--
+
+LOCK TABLES `ticket_comments` WRITE;
+/*!40000 ALTER TABLE `ticket_comments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ticket_comments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `ticket_dependencies`
+--
+
+LOCK TABLES `ticket_dependencies` WRITE;
+/*!40000 ALTER TABLE `ticket_dependencies` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ticket_dependencies` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Dumping data for table `tickets`
@@ -103,7 +77,6 @@ CREATE TABLE `tickets` (
 
 LOCK TABLES `tickets` WRITE;
 /*!40000 ALTER TABLE `tickets` DISABLE KEYS */;
-INSERT INTO `tickets` VALUES (1,'Connexion Wi-Fi impossible','Lutilisateur ne peut pas se connecter au Wi-Fi.','open','low',1,NULL,1,'2025-01-30 18:45:08','2025-01-30 18:45:08');
 /*!40000 ALTER TABLE `tickets` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -116,4 +89,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-01-30 20:22:42
+-- Dump completed on 2025-02-04 13:43:35
